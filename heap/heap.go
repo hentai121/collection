0ef25c0fdda7af459dab0f3b663f1aec18882a11
UerAx <uerax@live.com>
2022-05-14 01:33:38 +0800

    修改小顶对实现

--- heap/heap.go
+++ heap/heap.go
@@ -1,79 +1,89 @@
 package heap
 
 type MinHeap struct {
-	Heap []int
+	data []int
+	size int
 }
 
-func NewMinHeap(heap []int) *MinHeap {
-	m := &MinHeap{heap}
-	m.build()
-	return m
-}
-
-func (mh *MinHeap) Push(value int) {
-	mh.Heap = append(mh.Heap, value)
-	mh.siftUp(len(mh.Heap) - 1)
-}
-
-func (mh *MinHeap) Pop() int {
-	if len(mh.Heap) == 0 {
-		return 0
+func (this *MinHeap) AddAll(arr []int) {
+	if len(this.data) == 0 {
+		if len(arr) <= this.size {
+			this.data = append(this.data, arr...)
+			arr = arr[0:0]
+		} else {
+			this.data = append(this.data, arr[:this.size]...)
+			arr = arr[this.size:]
+		}
 	}
-	res := mh.Heap[0]
-	mh.Heap[0] = mh.Heap[len(mh.Heap)-1]
-	mh.Heap = mh.Heap[:len(mh.Heap)-1]
-	mh.siftDown(0, len(mh.Heap)>>1-1)
+	this.adjustAll()
+	for _, v := range arr {
+		this.add(v)
+	}
+}
+
+func (this *MinHeap) add(val int) {
+	if this.isFull() {
+		this.data[0] = val
+		this.adjustDown(0)
+	} else {
+		this.data = append(this.data, val)
+		this.adjustUp(this.len())
+	}
+}
+
+func (this *MinHeap) pop() int {
+	res := this.data[0]
+	this.swap(0, this.len()-1)
+	this.data = this.data[:this.len()-1]
+	this.adjustDown(0)
 	return res
 }
 
-func (mh *MinHeap) build() {
-	last := len(mh.Heap)>>1 - 1
-	for i := last; i >= 0; i-- {
-		left, right := 2*i+1, 2*i+2
-		if mh.Heap[i] > mh.Heap[left] {
-			swap(i, left, mh.Heap)
-			if left <= last {
-				mh.siftDown(left, last)
-			}
-		}
-		// left不会越界,防止right越界
-		if right < len(mh.Heap) && mh.Heap[i] > mh.Heap[right] {
-			swap(i, right, mh.Heap)
-			if right <= last {
-				mh.siftDown(right, last)
-			}
+func (this *MinHeap) adjustAll() {
+	for i := this.len() - 1; i > 0; i-- {
+		if this.data[i] < this.data[(i-1)/2] {
+			this.swap(i, (i-1)/2)
+			this.adjustDown(i)
 		}
 	}
 }
 
-func (mh *MinHeap) siftUp(pos int) {
-	for pos > 0 {
-		parent := (pos - 1) / 2
-		if mh.Heap[pos] > mh.Heap[parent] {
-			break
-		}
-		swap(pos, parent, mh.Heap)
-		pos = parent
+func (this *MinHeap) adjustUp(index int) {
+	if index < 0 {
+		return
+	}
+	if this.data[index] < this.data[(index-1)/2] {
+		this.swap(index, (index-1)/2)
+		this.adjustUp((index - 1) / 2)
 	}
 }
 
-func (mh *MinHeap) siftDown(pos, end int) {
-	l, r := 2*pos+1, 2*pos+2
-	if mh.Heap[pos] > mh.Heap[l] {
-		swap(pos, l, mh.Heap)
-		if l <= end {
-			mh.siftDown(l, end)
-		}
+func (this *MinHeap) adjustDown(index int) {
+	if 2*index+1 >= this.len() {
+		return
 	}
-	// left不会越界,防止right越界
-	if r < len(mh.Heap) && mh.Heap[pos] > mh.Heap[r] {
-		swap(pos, r, mh.Heap)
-		if r <= end {
-			mh.siftDown(r, end)
-		}
+	son := 2*index + 1
+	if 2*index+2 < this.len() && this.data[2*index+2] < this.data[son] {
+		son = 2*index + 2
 	}
+	if this.data[index] > this.data[son] {
+		this.swap(index, son)
+	}
+	this.adjustDown(son)
 }
 
-func swap(x, y int, heap []int) {
-	heap[x], heap[y] = heap[y], heap[x]
+func (this *MinHeap) len() int {
+	return len(this.data)
+}
+
+func (this *MinHeap) isFull() bool {
+	return len(this.data) == this.size
+}
+
+func (this *MinHeap) swap(s1, s2 int) {
+	this.data[s1], this.data[s2] = this.data[s2], this.data[s1]
+}
+
+func NewMinHeap(size int) *MinHeap {
+	return &MinHeap{make([]int, 0, size), size}
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -12,3 +12,27 @@ func TestStack(t *testing.T) {
 	fmt.Println(stack.Pop())
 	fmt.Println(stack.Geek())
 }
+
+func TestStack_Geek(t *testing.T) {
+	tests := []struct {
+		name    string
+		s       *Stack
+		want    int
+		wantErr bool
+	}{
+		{"test1", &Stack{[]int{1, 2, 3}}, 1, false},
+		{"test1", &Stack{[]int{3, 4, 5}}, 3, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.s.Geek()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Stack.Geek() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Stack.Geek() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
